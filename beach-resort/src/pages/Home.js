d814424893be6c52e8fdb29fe1cb1bb395c4c65d
Adarsh Pandey <adarshpandey022@gmail.com>
2021-03-07 00:21:46 +0530

    pages setup

--- beach-resort/src/pages/Home.js
+++ beach-resort/src/pages/Home.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title="luxurious Rooms" subtitle="Deluxe Room starting at $299">
+        <Link to="/rooms" className="btn-primary">
+          Our Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
