d5f17f1e5f963d314300dbd688d59cefbbcc78f4
Adarsh Pandey <adarshpandey022@gmail.com>
2021-03-14 15:35:32 +0530

    single page

--- beach-resort/src/components/Room.js
+++ beach-resort/src/components/Room.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import defaultImg from "../images/room-1.jpeg";
+import PropTypes from "prop-types";
 
 export default function Room({ room }) {
   const { name, slug, images, price } = room;
@@ -7,8 +9,25 @@ export default function Room({ room }) {
   return (
     <article className="room">
       <div className="img-container">
-        <img src={images[0]} alt="Singel-Room" />
+        <img src={images[0] || defaultImg} alt="Singel-Room" />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>Per Night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
       </div>
+      <p className="room-info">{name}</p>
     </article>
   );
 }
+
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired,
+  }),
+};
--- beach-resort/src/pages/SingleRooms.js
+++ beach-resort/src/pages/SingleRooms.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../Context";
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
+  }
+
+  static contextType = RoomContext;
+
   render() {
-    return <div>Hello from single room page</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No such rooms could be found</h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to Rooms
+          </Link>
+        </div>
+      );
+    }
+
+    return <div>Hello from single room page{room.name}</div>;
   }
 }
